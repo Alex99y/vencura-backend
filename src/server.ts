d6d6ef853235980ca0b18e69d8d4fe13af2b779c
Alexander <alex@stable.io>
2026-01-16 10:14:42 -0400

    Format

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,9 @@ app.use(
     })
 );
 
-app.use(rateLimit({...config.rateLimit, validate: { xForwardedForHeader: false }}));
+app.use(
+    rateLimit({ ...config.rateLimit, validate: { xForwardedForHeader: false } })
+);
 
 app.use(morgan('dev', { stream: morganStream }));
 
